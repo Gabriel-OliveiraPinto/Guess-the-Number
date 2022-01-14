9aa657d629a331e2e60a0954e7c25807d7030e84
Gabriel Pinto <67431182+Mamulengo98@users.noreply.github.com>
2022-01-14 17:13:34 -0300

    Add files via upload
    Rounds and attempts added. Lacking score and points based on attempts

---
+++ GuesstheNumber.java
@@ -0,0 +1,56 @@
+
+import javax.swing.*;
+ 
+public class GuesstheNumber {
+    public static void main(String[] args) {
+    	int rounds = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha o numero de rounds"
+        		, "Rounds", 3));
+    	while(rounds > 0){
+        int computerNumber = (int) (Math.random()*100 + 1);
+        int userAnswer = 0;
+        System.out.println("The correct guess would be " + computerNumber);
+        int count = 1;
+        int attempts = 3;
+        
+        while ((userAnswer != computerNumber) & attempts > 0)
+        {  
+          try{ 
+        	   
+        	   String response = JOptionPane.showInputDialog(null,
+                "Enter a guess between 1 and 100", "Guessing Game", 3);
+        	   userAnswer = Integer.parseInt(response);
+        	   JOptionPane.showMessageDialog(null, ""+ determineGuess(userAnswer, computerNumber, count));
+           }
+          catch(Exception e){
+        	  System.out.println(e);
+        	   
+        	   
+           }
+            count++;
+            attempts--;
+            
+         
+        }
+        rounds--; 
+        
+    	}
+    }
+
+    public static String determineGuess(int userAnswer, int computerNumber, int count){
+        if (userAnswer <=0 || userAnswer >100) {
+            return "Your guess is invalid";
+        }
+        else if (userAnswer == computerNumber ){
+            return "Correct!\nTotal Guesses: " + count;
+        }
+        else if (userAnswer > computerNumber) {
+            return "Your guess is too high, try again.\nTry Number: " + count;
+        }
+        else if (userAnswer < computerNumber) {
+            return "Your guess is too low, try again.\nTry Number: " + count;
+        }
+        else {
+            return "Your guess is incorrect\nTry Number: " + count;
+        }
+    }
+}
